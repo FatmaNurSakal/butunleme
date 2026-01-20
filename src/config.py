4d05ecc1f300bc6d443c1b2b3653ff1578494594
FatmaNurSakal <114931797+FatmaNurSakal@users.noreply.github.com>
2026-01-20 14:19:35 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ class TrainConfig:
     # data
     data_dir: str = "data"
     num_workers: int = 2
-    img_size: int = 224  # ✅ tüm modeller için ortak giriş boyutu
+    img_size: int = 224  # tüm modeller için ortak giriş boyutu
 
     # model
     model_name: str = "resnet18"  # simplecnn | customcnn | resnet18 | vit_tiny | convnext_tiny
@@ -13,7 +13,7 @@ class TrainConfig:
 
     # training
     seed: int = 42
-    epochs: int = 3          # ✅ deneme için
+    epochs: int = 3          # deneme için
     batch_size: int = 128
     lr: float = 1e-3
     weight_decay: float = 5e-4
@@ -21,3 +21,4 @@ class TrainConfig:
 
     # device
     device: str = "auto"  # "auto" | "cpu" | "cuda"
+
